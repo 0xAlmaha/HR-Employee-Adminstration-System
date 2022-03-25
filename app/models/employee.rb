e31855fe10800c123af4de85f884a506f2829029
Almaha <almaha.minwer@gmail.com>
2022-03-25 03:35:37 +0300

    test cases on emoployee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,10 +3,10 @@ class Employee < ApplicationRecord
     has_one :Division
     has_one :Team
 
-    validates! :salary,presence:true
-    # numericality: { greater_than_or_equal_to: 0 }
     validates :name, presence:true
     validates :job, presence:true
+    validates :salary, presence:true
+    # numericality: { greater_than_or_equal_to: 0 }
     validates :employment_status, presence:true
 
 end
